f45f368ba348c8e639eb7e1f4802139919a1cb12
Irma <irblancofdz@gmail.com>
2018-04-23 00:00:00 +0200

    Setup basic controllers

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by Irma Blanco on 22/04/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    //MARK:- Setup Functions
+    
+    func setupViewControllers() {
+        viewControllers = [
+            
+            generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- Helper Functions
+    
+    fileprivate func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
+    }
+}
